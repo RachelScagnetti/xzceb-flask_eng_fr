3401126ea6c0ff0c331dd0cc63bc6c50a9d67ae2
rachel.scagnetti <rachel.scagnetti@gmail.com>
2022-07-13 00:00:25 +0000

    Temporary changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -9,8 +9,14 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-authenticator = IAMAuthenticator('{apikey}')
+authenticator = IAMAuthenticator('apikey')
 language_translator = LanguageTranslatorV3(
     version='{2018-05-01}',
     authenticator=authenticator
 )
+
+language_translator.set_service_url('https://api.us-east.language-translator.watson.cloud.ibm.com')
+
+def englishToFrench(englishText):
+    french = language_translator.translate(englishText, model_id='en-fr')
+    return french
