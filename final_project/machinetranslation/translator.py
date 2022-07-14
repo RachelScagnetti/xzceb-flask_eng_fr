852c33ba793cad11d7a30feb40c4603ea062defb
Rachel <rachel.scagnetti@gmail.com>
2022-07-14 11:53:09 +0000

    Temporary changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,4 +1,4 @@
-"""Test"""
+"""Work with Watson Translator API"""
 import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
@@ -19,18 +19,18 @@ language_translator = LanguageTranslatorV3(
 language_translator.set_service_url(
     'https://api.us-east.language-translator.watson.cloud.ibm.com/instances/dafffe6c-acb7-4f34-8450-3ea60877a3d4')
 
-def english_to_french(englishText):
+def english_to_french(english_text):
     """English to French translation"""
     response = language_translator.translate(
-        text=englishText,
+        text=english_text,
         model_id='en-fr').get_result()
     french_translation = response['translations'][0]['translation']
     return french_translation
 
-def french_to_english(frenchText):
+def french_to_english(french_text):
     """French to English translation"""
     response = language_translator.translate(
-        text='Bonjour',
+        text=french_text,
         model_id='fr-en').get_result()
     english_translation = response['translations'][0]['translation']
     return english_translation
--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,28 @@
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
+import machinetranslation
 
 app = Flask("Web Translator")
 
-@app.route("/englishToFrench")
-def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
+@app.route("/english_to_french")
+def english_to_french():
+    english_text = request.args.get()
     # Write your code here
-    return "Translated text to French"
+    french_translation = translator.english_to_french(english_text)
+    return french_translation
 
-@app.route("/frenchToEnglish")
-def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
+@app.route("/french_to_english")
+def french_to_english():
+    french_text = request.args.get()
     # Write your code here
-    return "Translated text to English"
+    english_translation = translator.french_to_english(french_text)
+    return english_translation
 
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
