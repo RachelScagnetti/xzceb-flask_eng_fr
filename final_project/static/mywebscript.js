716e007bc9ffac37df341b3a305d452d330afd32
Rachel <rachel.scagnetti@gmail.com>
2022-07-14 16:22:26 +0000

    changes to js

--- final_project/static/mywebscript.js
+++ final_project/static/mywebscript.js
@@ -4,14 +4,14 @@ let english_to_french = ()=>{
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("english_to_french").innerHTML = xhttp.responseText;
+            document.getElementById("french_translation").innerHTML = xhttp.responseText;
         }
     };
-    xhttp.open("GET", "english_to_french?english_text"+"="+english_text, true);
+    xhttp.open("GET", "english_to_french?french_translation"+"="+english_text, true);
     xhttp.send();
 }
 
-let english_translation = ()=>{
+let french_to_english = ()=>{
     french_text = document.getElementById("french_text").value;
 
     let xhttp = new XMLHttpRequest();
@@ -20,7 +20,7 @@ let english_translation = ()=>{
             document.getElementById("english_translation").innerHTML = xhttp.responseText;
         }
     };
-    xhttp.open("GET", "french_to_english?french_text"+"="+french_text, true);
+    xhttp.open("GET", "french_to_english?english_translation"+"="+french_text, true);
     xhttp.send();
 }
 
