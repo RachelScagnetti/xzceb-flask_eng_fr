b6e6d8fe4ad81f9fc699e75df56009d364229a6a
Rachel <rachel.scagnetti@gmail.com>
2022-07-14 16:00:11 +0000

    temporary changes

--- final_project/static/mywebscript.js
+++ final_project/static/mywebscript.js
@@ -1,26 +1,26 @@
-let translateToFrench = ()=>{
-    textToTranslate = document.getElementById("textToTranslate").value;
+let english_to_french = ()=>{
+    english_text = document.getElementById("english_text").value;
 
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("translated_text").innerHTML = xhttp.responseText;
+            document.getElementById("english_to_french").innerHTML = xhttp.responseText;
         }
     };
-    xhttp.open("GET", "englishToFrench?textToTranslate"+"="+textToTranslate, true);
+    xhttp.open("GET", "english_to_french?english_text"+"="+english_text, true);
     xhttp.send();
 }
 
-let translateToEnglish = ()=>{
-    textToTranslate = document.getElementById("textToTranslate").value;
+let english_translation = ()=>{
+    french_text = document.getElementById("french_text").value;
 
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("translated_text").innerHTML = xhttp.responseText;
+            document.getElementById("english_translation").innerHTML = xhttp.responseText;
         }
     };
-    xhttp.open("GET", "frenchToEnglish?textToTranslate"+"="+textToTranslate, true);
+    xhttp.open("GET", "french_to_english?french_text"+"="+french_text, true);
     xhttp.send();
 }
 
